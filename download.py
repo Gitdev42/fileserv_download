245f7988bc5cac72d084a17b05e6083f4d40e02e
Julian Schuler <31921487+julianschuler@users.noreply.github.com>
2020-08-01 18:42:20 +0200

    Create recursive directory search

--- download.py
+++ download.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 
-import requests
+import os, requests
+try: 
+    from BeautifulSoup import BeautifulSoup
+except ImportError:
+    from bs4 import BeautifulSoup
 
+
+rootUrl = 'http://tuhh.fileserv.eu/thanks/'
 cookie = 'd8e0b842ea18876511e49e4ac8ae74b8'
 
 
@@ -15,13 +21,39 @@ def getFile(session, url):
 		print('Error: Could not access server. Please create a new cookie.')
 
 
+def downloadFiles(session, path, url):
+	# parse html
+	r = session.get(rootUrl + url)
+	soup = BeautifulSoup(r.text, "html.parser")
+
+	# create directory
+	dirName = path + url[5:]
+	if not os.path.exists(dirName):
+		try:
+			os.makedirs(dirName)
+		except OSError as exc: # Guard against race condition
+			if exc.errno != errno.EEXIST:
+				raise
+	
+	# download files in current directory
+	#for link in soup.find_all('a', {"class":"item file"}):
+	#	print(link.get('href'))
+	
+	# search through subdirectories recursively
+	for l in soup.find_all('a', {"class" : "item dir"}):
+		link = l.get('href')
+		print(link)
+		findDirs(session, path, link)
+
+
 def main():
+	# prepare session with authenication cookie
 	cookiejar = requests.cookies.cookiejar_from_dict({'fileserv2012' : cookie})
 	session = requests.Session()
 	session.cookies = cookiejar
 	
-	url='http://tuhh.fileserv.eu/thanks/Bachelor-MB-Pflichtmodule/Elektrische%20Maschinen/Klausuren/2008_09%20WS.pdf'
-	getFile(session, url)
+	# download all files
+	findDirs(session, "files/", "?dir=NTW-Bereich")
 
 
 main()
