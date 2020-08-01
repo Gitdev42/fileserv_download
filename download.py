d69be9c7b0a2469b73ed2927206f907f94f3926a
Julian Schuler <31921487+julianschuler@users.noreply.github.com>
2020-08-01 17:04:45 +0200

    Add hardcoded session cookie for file access

--- download.py
+++ download.py
@@ -1,17 +1,27 @@
+#!/usr/bin/python3
+
 import requests
 
-#try to get urls of files
-starturl = 'http://tuhh.fileserv.eu/thanks/?dir=Bachelor-MB-Pflichtmodule/Mathematik%20I/Lineare%20Algebra%20I/Klausuren'
-pagesource = requests.get(starturl, allow_redirects = True, auth=('student', 'tuhh'))
-print(pagesource.text) #debug help
-list = pagesource.text.split('href="') #search links in pagesource
-for item in list:
-    print(item[:item.find('"')])
+cookie = 'd8e0b842ea18876511e49e4ac8ae74b8'
 
-#download file from url and save with name from url
-url = 'http://tuhh.fileserv.eu/thanks/Bachelor-MB-Pflichtmodule/Mathematik%20I/Lineare%20Algebra%20I/Klausuren/klausur_la1%202006%20SS_A_lsg.pdf'
-r = requests.get(url, allow_redirects=True)
-substring1 = "/"
-filename = (url[url.rfind(substring1)+1:]) #extract filename
-open(filename, 'wb').write(r.content) #save file
-print('saved ' + filename)
+
+def getFile(session, url):
+	try:
+		r = session.get(url)
+		substring1 = "/"
+		filename = (url[url.rfind(substring1)+1:])
+		open(filename, 'wb').write(r.content)
+	except:
+		print('Error: Could not access server. Please create a new cookie.')
+
+
+def main():
+	cookiejar = requests.cookies.cookiejar_from_dict({'fileserv2012' : cookie})
+	session = requests.Session()
+	session.cookies = cookiejar
+	
+	url='http://tuhh.fileserv.eu/thanks/Bachelor-MB-Pflichtmodule/Elektrische%20Maschinen/Klausuren/2008_09%20WS.pdf'
+	getFile(session, url)
+
+
+main()
